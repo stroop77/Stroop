41074aea8217a3335acdc50ddf8ea43a5ba9e9b4
stroop <stroop666@163.com>
2016-04-29 21:25:56 +0800

    update 03

--- src/com/atguigu/stroop/HelloStroop.java
+++ src/com/atguigu/stroop/HelloStroop.java
@@ -5,5 +5,6 @@ public class HelloStroop {
 	public static void main(String[] args) {
 		System.out.println("Hello Stroop!");
 		System.out.println("update 01");
+		System.out.println("update 02");
 	}
 }
